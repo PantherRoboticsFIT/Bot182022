0d1c992793bdda760646c7f2dd78e7bed208e598
trainervolk7 <67025749+trainervolk7@users.noreply.github.com>
2023-01-28 14:36:59 -0500

    Initial commit for 18 inch bot 2022-2023 season

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "main.h"
+
+void on_center_button() {
+	
+}
+
+void initialize() {
+	
+}
+
+void disabled() {}
+
+void competition_initialize() {}
+
+void autonomous() {}
+
+void opcontrol() {
+	while (true) {
+		
+	}
+}
