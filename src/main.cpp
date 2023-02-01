4aa4cefbe7c7a5d4d3b697e54f40eeed7df6a897
trainervolk7 <67025749+trainervolk7@users.noreply.github.com>
2023-02-01 12:19:24 -0500

    Basic functions of 18 robot

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,17 @@ To do:
 */
 
 #include "main.h"
+#include <memory>
+#include <string>
 #define expansionPiston 'A'
+static int catapultAdust = 0;
 
 Motor intakeMotor {14,true,okapi::AbstractMotor::gearset::green,okapi::AbstractMotor::encoderUnits::degrees};
 Motor catapultMotor {4,true,okapi::AbstractMotor::gearset::red,okapi::AbstractMotor::encoderUnits::degrees};
 Motor rollerMotor {5,true,okapi::AbstractMotor::gearset::red,okapi::AbstractMotor::encoderUnits::degrees};
 
-auto r = RotationSensor(15);
+auto r = RotationSensor(15,true);
+pros::ADIDigitalIn catapultLimit ('H');
 
 Controller controller;
 pros::ADIDigitalOut expansion (expansionPiston);
@@ -18,10 +22,12 @@ ControllerButton launchCatapultButton (ControllerDigital::L1);
 ControllerButton retractCatapultButton (ControllerDigital::L2);
 ControllerButton intakeButton (ControllerDigital::R2);   
 ControllerButton reverseIntakeButton (ControllerDigital::R1); 
-//ControllerButton rollerButton (ControllerDigital::Y);
+ControllerButton rollerButton (ControllerDigital::X);
 //ControllerButton reverseRollerButton (ControllerDigital::B);//may not be needed/wanted
 ControllerButton expansionButton (ControllerDigital::up);
 ControllerButton expansionFailsafeButton (ControllerDigital::down);
+
+bool armset =true;
 
 std::shared_ptr<OdomChassisController> drive =
 	ChassisControllerBuilder()
@@ -41,6 +47,10 @@ std::shared_ptr<AsyncPositionController<double, double>> catapultControl =
 	AsyncPosControllerBuilder()
 		.withMotor(catapultMotor)
 		.build();
+std::shared_ptr<AsyncController<double, double>> catapultVelocityControl=
+	AsyncVelControllerBuilder()
+	.withMotor(catapultMotor)
+	.build();
 
 void on_center_button() {
 	
@@ -57,10 +67,12 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {}
+double degrees;
+std::string str;
 
 void opcontrol() {
 	while (true) {
-		drive->getModel()->arcade(controller.getAnalog(ControllerAnalog::leftY),controller.getAnalog(ControllerAnalog::rightX));
+		drive->getModel()->arcade(controller.getAnalog(ControllerAnalog::leftY),controller.getAnalog(ControllerAnalog::rightX)*0.75);
 		
 		if (intakeButton.changedToPressed())
 		{
@@ -79,14 +91,64 @@ void opcontrol() {
 		{
 			intakeMotor.moveVoltage(0);
 		}
+
+		if (rollerButton.changedToPressed())
+		{
+			rollerMotor.moveVelocity(-200);
+		}
+		else if(rollerButton.changedToReleased())
+		{
+			rollerMotor.moveVoltage(0);
+		}
 		
 		if (retractCatapultButton.isPressed())
 		{
-			//catapultControl->setTarget(-87);
+			//degrees = r.controllerGet();
+			//str = std::to_string(degrees);
+			//controller.setText(1, 1, str);
+			//r.reset();
+			//while(r.controllerGet()<70){
+			//	catapultMotor.moveVelocity(-50);
+			//}
+			//catapultControl->setTarget(-650);
+			//degrees = r.controllerGet();
+			//str = std::to_string(degrees);
+			//controller.setText(1, 1, str);
+			armset=false;
+			/*while(!catapultLimit.get_value()){
+				catapultMotor.moveVelocity(-100);
+				catapultVelocityControl->setTarget(-100);
+			}*/
+			//catapultMotor.moveVelocity(0);
+			//catapultVelocityControl->setTarget(0);
+			
+		}
+		if(!armset && !catapultLimit.get_value()){
+			catapultMotor.moveVelocity(-100);
+		}else{
+			catapultMotor.moveVelocity(0);
+			armset=true;
 		}
 		if (launchCatapultButton.isPressed())
 		{
-			catapultControl->setTarget(0);
+			while(catapultLimit.get_value()){
+				catapultMotor.moveVelocity(-100);
+				//catapultVelocityControl->setTarget(-100);
+			}
+			catapultMotor.moveVelocity(0);
+			//catapultVelocityControl->setTarget(0);
+			//catapultMotor.moveRelative(-50, -50);
+			/*catapultControl->setTarget(-800);
+			catapultControl->waitUntilSettled();
+			pros::delay(500);
+			catapultControl->setTarget(-1050+catapultAdust);
+			catapultControl->waitUntilSettled();
+			pros::delay(1000);
+			catapultMotor.tarePosition();
+			catapultAdust-=100;*/
+			//while(r.get()<85){
+			//	catapultMotor.moveVelocity(200);
+			//}
 		}
 	}
 }
